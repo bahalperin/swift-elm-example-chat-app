2644cae93d0b146bfcd03d1cd32bdfe05deead22
Ben Halperin <bahalperin@gmail.com>
2017-04-04 19:38:02 -0400

    Upgrade vapor

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "VaporElmChat",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 1)
         ],
     exclude: [
         "Config",
